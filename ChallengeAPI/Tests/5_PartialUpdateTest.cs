7d80a51e45f5f432c4b2f09316b902a31fe9e96e
Javier <torresjavier83@gmail.com>
2025-02-25 17:09:31 -0300

    Final commit

---
+++ ChallengeAPI/Tests/5_PartialUpdateTest.cs
@@ -0,0 +1,49 @@
+﻿using ChallengeAPI.Models;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeAPI.Tests
+{
+    public class PartialUpdateTest : TestBase
+    {
+        [Description("Partial Update Booking")]
+        [Category("TC_5"), Category("PartialUpdate"), Category("Scenario_01")]
+        [Test]
+        public void PartialUpdate_01()
+        {
+            Booking booking = new Booking("John", "Doe");
+
+            var response = Api.CreateBooking(booking);
+
+            var bookID = Api.GetJsonValue(response, "bookingid");
+            var token = Api.GetAuthToken(User.Valid.Username, User.Valid.Password);
+
+            var request = new RestRequest($"/booking/{bookID}", Method.Patch);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Cookie", $"token={token}");
+
+            var editedBody = new
+            {
+                firstname = "Edited"
+            };
+
+            request.AddJsonBody(editedBody);
+
+            var PatchResponse = Api.ExecuteRequest(request);
+
+            var firstName = Api.GetJsonValue(PatchResponse, "firstname");
+
+
+            Assert.That(PatchResponse.StatusCode == HttpStatusCode.OK);
+            Assert.That(firstName == "Edited");
+
+        }
+    }
+}
